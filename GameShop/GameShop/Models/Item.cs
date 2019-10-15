764547b753a3e22886354ef65eef8d815ac13242
Jordan Forde <S00181590@mail.itsligo.ie>
2019-10-15 10:56:31 +0100

    Model and Controllers set up

---
+++ GameShop/GameShop/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameShop.Models
+{
+    public enum ItemCategory { Health, Stamina, Magic};
+
+    public class Item
+    {
+        public ItemCategory Category { get; set; }
+
+        [DataType("decimal(18,2)")]
+        public double Cost { get; set; }
+
+        public string IconURL { get; set; }
+
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
